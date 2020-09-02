da208797bb5b4b07b7e858b74977e3cdba7c2c17
JAISAPPAN <67114455+JAISAPPAN@users.noreply.github.com>
2020-09-02 07:59:58 +0530

    Add files via upload

---
+++ build/precache-manifest.643ffa3f5d1b5233847ae34e6e439c3b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fc5af6d074d27a93d5b66b2a907a80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221331fa3d252333b1f0",
+    "url": "/static/css/2.dba7c812.chunk.css"
+  },
+  {
+    "revision": "dfe6fa7bc14ae612f624",
+    "url": "/static/css/main.946a9c08.chunk.css"
+  },
+  {
+    "revision": "221331fa3d252333b1f0",
+    "url": "/static/js/2.62927832.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.62927832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe6fa7bc14ae612f624",
+    "url": "/static/js/main.895cc73b.chunk.js"
+  },
+  {
+    "revision": "957289e1406dd8317895",
+    "url": "/static/js/runtime-main.801b4285.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
